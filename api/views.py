49b66f67092ac3e09f1026d069cbe95b6a236cb1
karlokarlo87 <karlo.piranishvili@gmail.com>
2024-11-10 01:17:51 +0400

    add

--- api/views.py
+++ api/views.py
@@ -8,10 +8,18 @@ import azure.cognitiveservices.speech as speechsdk
 from django.conf import settings
 from django.templatetags.static import static
 import os
+from pydub import AudioSegment
+from pydub.playback import play
+
 # Create your views here.
 @csrf_exempt
 def get_voice(request):
     return JsonResponse({"sdfsd":'dfgdfgdfg'})
+
+def play_audio_from_bytes(audio_data,format='wave'):
+    audio_Data_io = io.BytesIO(audio_data)
+    audio_Segment = AudioSegment.from_file(audio_Data_io, format=format)
+    play(audio_Segment)
 @csrf_exempt
 def my_api_view(request):
     if request.method == "GET":  # or 'GET'
@@ -20,24 +28,28 @@ def my_api_view(request):
         speech_config = speechsdk.SpeechConfig(subscription=subscription_key, region=region)
         speech_config.speech_synthesis_voice_name = "ka-GE-EkaNeural"
 
-        audio_stream = io.BytesIO()
-
         # Set up the audio output configuration to use the in-memory stream
         audio_config = speechsdk.audio.AudioOutputConfig(use_default_speaker=True)
-        static_file_path = os.path.join(settings.BASE_DIR, 'staticfiles', 'temp.wav')
         # Synthesize speech
         synthesizer = speechsdk.SpeechSynthesizer(speech_config=speech_config, audio_config=audio_config)
-        result = synthesizer.speak_text_async('და').get()
-        audio_data_stream = speechsdk.AudioDataStream(result)
-        audio_data_stream.save_to_wav_file(static_file_path)
-        # Check the result of speech synthesis (you can also handle errors here)
+        result = synthesizer.speak_text_async('და')
+        if result.get().reason == synthesizer.ResultReason.SynthesizingAudioCompleted:
+            result.get()
+            audio_data = result.get().audio_data
+            play_audio_from_bytes(audio_data)
+            with open("output.wave", "wb") as file:
+                file.write(audio_data)
 
-        # Rewind to the start of the stream
-        audio_stream.seek(0)
+            audio_stream = io.BytesIO()
 
-        # Send the audio data as a response for download
-        response = HttpResponse(audio_stream, content_type='audio/wav')
-        response['Content-Disposition'] = 'attachment; filename="output.wav"'
-        return response
+            # Check the result of speech synthesis (you can also handle errors here)
+
+            # Rewind to the start of the stream
+            audio_stream.seek(0)
+
+            # Send the audio data as a response for download
+            response = HttpResponse(audio_stream, content_type='audio/wave')
+            response['Content-Disposition'] = 'attachment; filename="output.wave"'
+            return response
 
 
