bcedfa4746510e781c22233109c5772b1f43b77a
karlokarlo87 <karlo.piranishvili@gmail.com>
2024-11-09 22:16:56 +0400

    add

--- api/views.py
+++ api/views.py
@@ -1,3 +1,10 @@
-from django.shortcuts import render
+from django.views.decorators.csrf import csrf_exempt
+from rest_framework.decorators import api_view
+from django.http import HttpResponse, JsonResponse
+import io
+import azure.cognitiveservices.speech as speechsdk
 
 # Create your views here.
+@csrf_exempt
+def get_voice(request):
+    return JsonResponse({"sdfsd":'dfgdfgdfg'})
